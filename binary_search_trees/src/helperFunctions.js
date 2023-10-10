7aeeafee0238c54c4ddf55fc01b8bb1d93038ba6
jiankh <131821436+jiankh@users.noreply.github.com>
2023-10-10 15:17:08 -0400

    refactoring

---
+++ binary_search_trees/src/helperFunctions.js
@@ -0,0 +1,42 @@
+
+
+const prettyPrint = (node, prefix = "", isLeft = true) => {
+    if (node === null) {
+      return;
+    }
+    if (node.right !== null) {
+      prettyPrint(node.right, `${prefix}${isLeft ? "│   " : "    "}`, false);
+    }
+    console.log(`${prefix}${isLeft ? "└── " : "┌── "}${node.data}`);
+    if (node.left !== null) {
+      prettyPrint(node.left, `${prefix}${isLeft ? "    " : "│   "}`, true);
+    }
+};
+
+const prettyHTML = (node, prefix = "", isLeft = true) => {
+    if (node === null) {
+        return "";
+    }
+    let result = "";
+    if (node.right !== null) {
+        result += prettyHTML(node.right, `${prefix}${isLeft ? "│   " : "    "}`, false);
+    }
+    result += `${prefix}${isLeft ? "└── " : "┌── "}${node.data}\n`;
+    if (node.left !== null) {
+        result += prettyHTML(node.left, `${prefix}${isLeft ? "    " : "│   "}`, true);
+    }
+    return result;
+};
+
+
+function getHeight(node) {
+    if (node === null) {
+        return 0;
+    } else {
+        const leftHeight = getHeight(node.left);
+        const rightHeight = getHeight(node.right);
+        return Math.max(leftHeight, rightHeight) + 1;
+    }
+}
+
+export {getHeight,prettyHTML, prettyPrint}
--- binary_search_trees/src/index.js
+++ binary_search_trees/src/index.js
@@ -1,227 +1,7 @@
-const main = document.querySelector("body")
-const screen = document.createElement('div')
+import {Tree} from "./Tree"
+import {getHeight,prettyHTML, prettyPrint} from "./helperFunctions"
 
 
-class Node{
-    constructor(data) {
-        this.data = data
-        this.left = null
-        this.right = null
-    }
-
-}
-
-class Tree {
-  constructor(arr) {
-    const sortedArray = [...new Set(arr)].sort((a, b) => a - b); //removes duplicates and sorts it
-    this.root = this.buildTree(sortedArray); //as soon as an obj is made w/ class Tree, will trigger buildTree()
-  }
-
-  buildTree(sortedArray) {
-    //takes arr and build a balanced tree full of nodes
-
-    if (sortedArray.length === 0) {
-      return null;
-    } //check if there is even data
-    const middleIndex = Math.floor(sortedArray.length / 2);
-    const leftSideArr = sortedArray.slice(0, middleIndex);
-    const rightSideArr = sortedArray.slice(middleIndex + 1);
-
-    const root = new Node(sortedArray[middleIndex]);
-    root.left = this.buildTree(leftSideArr);
-    root.right = this.buildTree(rightSideArr);
-
-    return root;
-  }
-
-  insertNode(data) {
-    let node = new Node(data);
-
-    if (this.root == null) {
-      this.root = node;
-      return;
-    } else {
-      let prev = null;
-      let temp = this.root;
-
-      while (temp !== null) {
-        prev = temp;
-        if (data < temp.data) {
-          temp = temp.left;
-        } else if (data > temp.data) {
-          temp = temp.right;
-        } else {
-          // Duplicate value, skip insertion
-          return;
-        }
-      }
-
-      // Now that we've found the correct position, insert the new node.
-      if (data < prev.data) {
-        prev.left = node;
-      } else {
-        prev.right = node;
-      }
-    }
-  }
-
-  findMinValueNode(node) {
-    let current = node;
-    while (current.left !== null) {
-        current = current.left;
-    }
-    return current;
-  }
-
-  deleteNode(root, k) {
-    // Base case, no root so no tree so nothing to delete
-    if (root === null) {
-      return root;
-    }
-   
-    // Recursive calls for ancestors, go thur the tree to find where data = k
-    if (root.data > k) {
-      root.left = this.deleteNode(root.left, k);
-      return root; //if it reaches null from line above then is not in this branch and return the original root
-    } else if (root.data < k) {
-      root.right = this.deleteNode(root.right, k);
-      return root;
-    }
-   
-    // We reach here when root is the node
-    // to be deleted. When the call gets here it means root.data = k
-   
-    // If one of the children is empty
-    if (root.left === null) {
-      return root.right //this will set the left/right of the node above level to this(Deleting the current node)(check the recursive call above this)
-    } else if (root.right === null) {
-      return root.left
-    }
-   
-    // If both children exist
-    // Node with two children, find the inorder successor (smallest in the right subtree)
-    let minValueNode = this.findMinValueNode(root.right);
-
-    // Copy the inorder successor's content to this node
-    root.data = minValueNode.data;
-
-    // Delete the inorder successor
-    root.right = this.deleteNode(root.right, minValueNode.data);
-
-    return root;
-   
-  
-  }
-
-  breadthFirstValues() {
-    const root = this.root
-    if (root === null) return [] //if no root then no tree then just return empty array
-
-    const values = [] //initialize a array to store final values
-    const queue =[root] //breadthfirst uses a queue system
-
-    while (queue.length > 0) {
-      const current = queue.shift() //shift takes the first index and returns it
-      values.push(current.data)
-
-      //we push the subtrees of the current node into the queue
-      if (current.left !== null) {queue.push(current.left)}
-      if (current.right !== null) {queue.push(current.right)}
-    }
-    return values
-  }
-
-  preorder(root=this.root) {
-    if (root === null)
-    return [];
-  
-    const leftValues = this.preorder(root.left);
-    const rightValues = this.preorder(root.right);
-    return [ root.data, ...leftValues, ...rightValues ];
-  }
-
-  inorder(root=this.root) {
-    if (root === null)
-    return [];
-  
-    const leftValues = this.inorder(root.left);
-    const rightValues = this.inorder(root.right);
-    return [ ...leftValues, root.data,  ...rightValues ];
-  }
-
-  postorder(root=this.root) {
-    if (root === null)
-    return [];
-  
-    const leftValues = this.postorder(root.left);
-    const rightValues = this.postorder(root.right);
-    return [ ...leftValues,  ...rightValues, root.data];
-  }
-  
-}
-
-function getHeight(node) {
-  if (node === null) {
-      return 0;
-  } else {
-      const leftHeight = getHeight(node.left);
-      const rightHeight = getHeight(node.right);
-      return Math.max(leftHeight, rightHeight) + 1;
-  }
-}
-
-
-
-
-
-
-
-
-const randomArray = (size) => {
-    return Array.from({ length: size }, () => Math.floor(Math.random() * 100));
-  }
-
-const depthFirstValues = (root) => {
-    if (root === null)
-      return [];
-    
-    const leftValues = depthFirstValues(root.left);
-    const rightValues = depthFirstValues(root.right);
-    return [ root.data, ...leftValues, ...rightValues ];
-} 
-
-const prettyPrint = (node, prefix = "", isLeft = true) => {
-    if (node === null) {
-      return;
-    }
-    if (node.right !== null) {
-      prettyPrint(node.right, `${prefix}${isLeft ? "│   " : "    "}`, false);
-    }
-    console.log(`${prefix}${isLeft ? "└── " : "┌── "}${node.data}`);
-    if (node.left !== null) {
-      prettyPrint(node.left, `${prefix}${isLeft ? "    " : "│   "}`, true);
-    }
-  };
-
-
-//     // A recursive function to insert a new key in BST
-//     function insertRec(root, key) {
-
-//     // If the tree is empty, return a new node
-//     if (root == null) {
-//         root = new Node(key);
-//         return root;
-//     }
-
-//     // Otherwise, recur down the tree
-//     if (key < root.key)
-//         root.left = insertRec(root.left, key);
-//     else if (key > root.key)
-//         root.right = insertRec(root.right, key);
-
-//     // Return the (unchanged) node pointer
-//     return root;
-// }
 
 
 const arr1 = [62, 8, 19, 92, 51, 52, 85, 28, 74, 0]
@@ -231,7 +11,12 @@ const tree = new Tree(arr1)
 // tree.insertNode(9)
 // tree.insertNode(999)
 // prettyPrint(tree.root)
-main.appendChild(screen)
+
+
+// main.appendChild(screen)
+
+
+
 
 // tree.deleteNode(tree.root,52)
 prettyPrint(tree.root)
@@ -240,3 +25,11 @@ console.log(`inorder: ${tree.inorder()}`)
 console.log(`preorder: ${tree.preorder()}`)
 console.log(`postorder: ${tree.postorder()}`)
 console.log(`Height: ${getHeight(tree.root)}`)
+
+
+
+const main = document.querySelector(".tree")
+// const screen = document.createElement('div')
+
+const prettyprint = prettyHTML(tree.root)
+main.textContent = prettyprint
